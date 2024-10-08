a5844c2d820cf87e8668958016a03fe7aaa4415d
segunemma2003 <segunemma2003@gmail.com>
2024-10-08 08:02:48 +0100

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,10 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./assets/**/*.{js,ts,jsx,tsx,mdx}",
-    "./context/**/*.{js,ts,jsx,tsx,mdx,json}",
-    "./data/**/*.{js,ts,jsx,tsx,mdx}",
-    "./utils/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./assets/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./context/**/*.{js,ts,jsx,tsx,mdx,json}",
+    // "./data/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./utils/**/*.{js,ts,jsx,tsx,mdx}",
  
     // Or if using `src` directory:
     // "./**/*.{js,ts,jsx,tsx,mdx}",
